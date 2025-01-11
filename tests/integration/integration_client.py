119787790b001d359e001592e10d1c4f67a6870c
Mikhail Tavarez <miktavarez@gmail.com>
2025-01-11 16:44:49 -0600

    Add more tests

---
+++ tests/integration/integration_client.py
@@ -0,0 +1,18 @@
+import requests
+
+
+# TODO: Pair with the Mojo integration server to test the client and server independently.
+print("\n~~~ Testing redirect ~~~")
+session = requests.Session()
+response = session.get('http://127.0.0.1:8000/redirect', allow_redirects=True)
+assert response.status_code == 200
+assert response.text == "yay you made it"
+
+print("\n~~~ Testing close connection ~~~")
+response = session.get('http://127.0.0.1:8000/close-connection', headers={'connection': 'close'})
+assert response.status_code == 200
+assert response.text == "connection closed"
+
+print("\n~~~ Testing internal server error ~~~")
+response = session.get('http://127.0.0.1:8000/error', headers={'connection': 'keep-alive'})
+assert response.status_code == 500
